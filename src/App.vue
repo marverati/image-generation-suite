b3790317b1dec86c62e00b7f40542c0f108efaf7
m.over <m.over@iplabs.de>
2021-02-10 19:32:46 +0100

    Basic grid layout

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,29 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <IGS />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import IGS from './components/IGS.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    IGS
   }
 });
 </script>
 
 <style lang="scss">
+body {
+  margin: 0;
+}
+
 #app {
+  height: 100vh;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/IGS.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="igs">
+    <!-- <ProjectTree class="grid-left" />
+    <CodingArea class="grid-a" />
+    <Parameters class="grid-b" />
+    <PreviewArea class="grid-c" /> -->
+    <div class="grid-left" />
+    <div class="grid-a" />
+    <div class="grid-b" />
+    <div class="grid-c" />
+  </div>
+</template>
+
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'HelloWorld',
+  props: {
+    msg: String,
+  },
+});
+</script>
+
+<style scoped lang="scss">
+
+.igs {
+  width: 100%;
+  height: 100%;
+  display: grid;
+  grid-template-areas: "l a c" "l b c";
+
+  div {
+    min-width: 100px;
+    min-height: 100px;
+  }
+}
+
+.grid-left {
+  grid-area: l;
+}
+
+.grid-a {
+  grid-area: a;
+  border: 1px solid gray;
+  border-top: none;
+}
+
+.grid-b {
+  grid-area: b;
+  border: 1px solid gray;
+  border-top: none;
+  border-bottom: none;
+}
+
+.grid-c {
+  grid-area: c;
+}
+
+</style>
