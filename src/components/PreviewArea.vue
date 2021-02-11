5c9b5fc6640e46cb9fc24b83936c7d1e18e75441
m.over <m.over@iplabs.de>
2021-02-11 09:17:12 +0100

    Add checkerboard pattern to canvas

--- src/components/PreviewArea.vue
+++ src/components/PreviewArea.vue
@@ -3,7 +3,14 @@
       <div class="content">
         <h1>Preview</h1>
         <div class="canvas-space">
-          <canvas ref="previewCanvas" width=512 height=512 @click="openPreview()" />
+          <div class="canvas-div">
+            <canvas ref="previewCanvas" width=812 height=512 @click="openPreview()"
+                :class="showBackground ? 'alpha-background' : ''" />
+            <!-- Canvas Setting -->
+            <div class="canvas-settings">
+              <button @click="toggleBackground()">{{toggleBackgroundLabel}}</button>
+            </div>
+          </div>
         </div>
       </div>
   </div>
@@ -19,6 +26,7 @@ export default defineComponent({
   components: {
   },
   data: () => { return {
+    showBackground: true
   }},
   computed: {
     canvas (): HTMLCanvasElement {
@@ -26,6 +34,9 @@ export default defineComponent({
     },
     context (): CanvasRenderingContext2D {
       return this.canvas.getContext("2d") as CanvasRenderingContext2D;
+    },
+    toggleBackgroundLabel (): string {
+      return this.showBackground ? "Hide Alpha Background" : "Show Alpha Background";
     }
   },
   props: {
@@ -42,6 +53,9 @@ export default defineComponent({
         this.$emit("openPreview");
       };
       img.src = this.canvas.toDataURL();
+    },
+    toggleBackground() {
+      this.showBackground = !this.showBackground;
     }
   },
   watch: {
@@ -64,13 +78,48 @@ export default defineComponent({
       flex-grow: 1;
       display: flex;
       justify-content: center;
-      canvas {
-        border: 1px solid #ccc;
-        box-shadow: 0 2px 4px #aaa;
-        max-height: 95%;
-        max-width: 95%;
+      position: relative;
+
+      .canvas-div {
         margin: auto;
         display: block;
+        max-width: 100%;
+        max-height: 80%;
+        
+        canvas {
+          // border: 1px solid #ccc;
+          box-shadow: 0 2px 4px #aaa;
+          max-height: 95%;
+          max-width: 95%;
+          margin: auto;
+          display: block;
+          cursor: pointer;
+
+          &.alpha-background {
+            // background-position: 0 0;
+            // background-repeat: repeat;
+            // background-size: 4%;
+            // background-image: url('~@/assets/checker.png');
+            background: repeating-conic-gradient(#808080 0% 25%, transparent 0% 50%) 
+              50% / 20px 20px
+          }
+        }
+
+        .canvas-settings {
+          display: block;
+          border: 1px solid #ccc;
+          border-radius: 2px;
+          margin: 12px auto;
+          padding: 4px;
+          position: absolute;
+          left: 50%;
+          transform: translate(-50%, 0);
+
+          button {
+            font-size: 60%;
+            cursor: pointer;
+          }
+        }
       }
     }
   }
