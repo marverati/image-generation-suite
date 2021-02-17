defb23b2abe693fb36a4b04e92197401b1762f2b
marverati <marverati@gmail.com>
2021-02-17 19:28:25 +0100

    Add interpolation option to color gradients

--- src/imaging/ColorGradient.ts
+++ src/imaging/ColorGradient.ts
@@ -2,23 +2,46 @@ import { exposeToWindow } from "@/util";
 import { blendColors, colorToRGBA } from "./color";
 import ColorMapping, { ColorMappingFunction, ColorMappingOverflow } from "./ColorMapping";
 import { ColorRGB, ColorRGBA } from "./imageUtil";
+import { Interpolation, InterpolationMode } from "./interpolation";
 
-type ColorList = (ColorRGBA | ColorRGB | number)[];
+type Color = number | ColorRGB | ColorRGBA;
+type ColorList = Color[];
+type ColorsObject = Record<number, Color>;
 
 export default class ColorGradient extends ColorMapping {
 
-    public constructor(public readonly colors: ColorList,
+    public constructor(public readonly colors: ColorList, interpolation: InterpolationMode = InterpolationMode.LINEAR,
             overflow?: ColorMappingOverflow, cacheSteps = colors.length * 256) {
-        super(ColorGradient.getColorMapping(colors), overflow, cacheSteps)
+        super(ColorGradient.getColorMapping(colors, interpolation), overflow, cacheSteps)
     }
 
-    public static getColorMapping(colors: ColorList): ColorMappingFunction {
-        const rgbaColors = colors.map(colorToRGBA);
-        const max = colors.length - 1;
-        return (p: number) => {
-            const floatingIndex = p * max, i1 = floatingIndex << 0, i2 = i1 + 1;
-            return blendColors(rgbaColors[i1], rgbaColors[i2], floatingIndex - i1);
-        };
+    public static getColorMapping(colors: ColorList | ColorsObject, interpolation: InterpolationMode): ColorMappingFunction {
+        const interpolator = Interpolation[interpolation];
+        if (colors instanceof Array) {
+            // Uniform list of colors
+            const rgbaColors = colors.map(colorToRGBA);
+            const max = colors.length - 1;
+            return (p: number) => {
+                const floatingIndex = p * max, i1 = floatingIndex << 0, i2 = i1 + 1;
+                return blendColors(rgbaColors[i1], rgbaColors[i2], interpolator(floatingIndex - i1));
+            };
+        } else {
+            // Object with float element of [0, 1] as keys, Color as value
+            const keys = Object.keys(colors).map(v => +v).filter(v => v >= 0 && v <= 1).sort((a, b) => a - b);
+            const rgbaColors = keys.map(key => colorToRGBA(colors[key]));
+            // store prevIndex to optimize search
+            let prevL = 0, prevR = 1, prevValue = keys[0], prevSpan = keys[1] - keys[0];
+            return (p: number) => {
+                if (keys[prevR] < p || keys[prevL] > p) {
+                    // New search required
+                    prevR = Math.max(1, keys.findIndex(v => v >= p));
+                    prevL = prevR - 1;
+                    prevValue = keys[prevL];
+                    prevSpan = keys[prevR] - prevValue;
+                }
+                return blendColors(rgbaColors[prevL], rgbaColors[prevR], interpolator((p - prevValue) / prevSpan));
+            };
+        }
     }
 }
 
--- src/imaging/interpolation.ts
+++ src/imaging/interpolation.ts
@@ -1,8 +1,20 @@
 import { exposeToWindow } from "@/util";
 
+export enum InterpolationMode {
+    LINEAR = "linear",
+    COS = "cos",
+    CUBIC = "cubic",
+    STEP = "step",
+    STEP0 = "step0",
+    STEP1 = "step1"
+}
+
 export const Interpolation = {
     linear: (v: number) => v,
     cos: (v: number) => v < 0 ? 0 : v > 1 ? 1 : 0.5 - 0.5 * Math.cos(Math.PI * v),
+    cos2: (v: number) => v < 0 ? 0 : v > 1 ? 1 : 0.5 - 0.5 * Math.cos(Math.PI * (0.5 - 0.5 * Math.cos(Math.PI * v))),
+    cos3: (v: number) => v < 0 ? 0 : v > 1 ? 1 : 0.5 - 0.5 * Math.cos(Math.PI
+            * (0.5 - 0.5 * Math.cos(Math.PI * (0.5 - 0.5 * Math.cos(Math.PI * v))))),
     cubic: (v: number) => v < 0 ? 0 : v > 1 ? 1 : 3 * v * v - 2 * v * v * v,
     step: (v: number) => v > 0.5 ? 1 : 0,
     step0: (v: number) => v > 0 ? 1 : 0,
