6166c36663a6a907a4f22f5dc171bc2dc5e60b22
marverati <marverati@gmail.com>
2021-02-25 21:33:45 +0100

    Basic buffered filtering via boolean parameter

--- src/imaging/filter.ts
+++ src/imaging/filter.ts
@@ -1,25 +1,38 @@
+/* eslint-disable @typescript-eslint/no-use-before-define */
+
 import { exposeToWindow } from "@/util";
 import { ColorRGB, ColorRGBA, currentCanvas, currentContext, getClipping } from "./imageUtil";
 
-export type Filter = ((c: ColorRGBA, x: number, y: number) => number)
-    | ((c: ColorRGBA, x: number, y: number) => ColorRGB)
-    | ((c: ColorRGBA, x: number, y: number) => ColorRGBA);
+export type Filter = ((c: ColorRGBA, x: number, y: number, get: GetColor) => number)
+    | ((c: ColorRGBA, x: number, y: number, get: GetColor) => ColorRGB)
+    | ((c: ColorRGBA, x: number, y: number, get: GetColor) => ColorRGBA);
 
-export function filter(filterFunc: Filter): void {
+export type GetColor = (x: number, y: number) => ColorRGBA;
+
+export enum BUFFER_TYPE {
+    NONE = "none",
+    BUFFER = "buffer"
+}
+
+export function filter(filterFunc: Filter, buffer = false): void {
     const w = currentCanvas.width, h = currentCanvas.height;
     const dataObj = currentContext.getImageData(0, 0, w, h);
     const data = dataObj.data;
     let p = 0;
+
+    const pixelGetter = prepareBufferOrGetter(buffer ? BUFFER_TYPE.BUFFER : BUFFER_TYPE.NONE, dataObj);
+
     const col: ColorRGBA = [0, 0, 0, 0];
-    const example = filterFunc(col, 0, 0);
+    const example = filterFunc(col, 0, 0, () => [0,0,0,255]);
     if (typeof example === "number") {
       const original = filterFunc;
       filterFunc = (col, x, y) => {
-        const c = original(col, x, y) as number;
+        const c = original(col, x, y, pixelGetter) as number;
         return [c, c, c, 255];
       }
     }
-    const typedFilterFunc = filterFunc as ((c: ColorRGBA, x: number, y: number) => ColorRGBA);
+    const typedFilterFunc = filterFunc as ((c: ColorRGBA, x: number, y: number, get: GetColor) => ColorRGBA);
+
     let result: ColorRGBA = col;
     const [clipX, clipY, clipW, clipH] = getClipping();
     const ymax = clipY + clipH, xmax = clipX + clipW;
@@ -30,7 +43,7 @@ export function filter(filterFunc: Filter): void {
         col[1] = data[p + 1];
         col[2] = data[p + 2];
         col[3] = data[p + 3];
-        result = typedFilterFunc(col, x, y);
+        result = typedFilterFunc(col, x, y, pixelGetter);
         data[p++] = result[0];
         data[p++] = result[1];
         data[p++] = result[2];
@@ -42,6 +55,27 @@ export function filter(filterFunc: Filter): void {
       }
     }
     currentContext.putImageData(dataObj, 0, 0);
+}
+
+function prepareBufferOrGetter(buffer: BUFFER_TYPE, imageData: ImageData): GetColor {
+    const data = imageData.data, w = imageData.width;
+    switch (buffer) {
+        case BUFFER_TYPE.NONE:
+            // No buffering -> just get pixel
+            return (x: number, y: number) => {
+                const p = 4 * (x + w * y);
+                return [ data[p], data[p + 1], data[p + 2], data[p + 3] ];
+            }
+        case BUFFER_TYPE.BUFFER:
+            // Simple buffering -> store whole image data
+            {
+                const buff = data.slice();
+                return (x: number, y: number) => {
+                    const p = 4 * (x + w * y);
+                    return [ buff[p], buff[p + 1], buff[p + 2], buff[p + 3] ];
+                }
+            }
+    }
 }
 
 exposeToWindow({
