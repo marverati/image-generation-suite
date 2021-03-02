d08294ba18c235acd50c6ba7c3cc9e6a235cd47b
marverati <marverati@gmail.com>
2021-03-02 18:44:08 +0100

    Support enhanced pixel getter functions for filtering, also distortion example

--- src/imaging/filter.ts
+++ src/imaging/filter.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-use-before-define */
 
-import { exposeToWindow } from "@/util";
+import { clamp, exposeToWindow } from "@/util";
 import { ColorRGB, ColorRGBA, currentCanvas, currentContext, getClipping } from "./imageUtil";
 
 export type Filter = ((c: ColorRGBA, x: number, y: number, get: GetColor) => number)
@@ -22,7 +22,7 @@ export enum BUFFER_TYPE {
 export function getPixelGetter(): GetColor {
     const w = currentCanvas.width, h = currentCanvas.height;
     const dataObj = currentContext.getImageData(0, 0, w, h);
-    return prepareBufferOrGetter(BUFFER_TYPE.NONE, dataObj);
+    return prepareBufferOrGetter(dataObj, false);
 }
 
 export function getChannelGetters(): [ GetNumber, GetNumber, GetNumber, GetNumber ] {
@@ -43,14 +43,14 @@ export function analyzePixels(analyzer: PixelAnalyzer): void {
     const ymax = clipY + clipH, xmax = clipX + clipW;
     const col: ColorRGBA = [0, 0, 0, 0];
     for (let y = clipY; y < ymax; y++) {
-      p = 4 * (clipX + w * y);
-      for (let x = clipX; x < xmax; x++) {
-        col[0] = data[p];
-        col[1] = data[p + 1];
-        col[2] = data[p + 2];
-        col[3] = data[p + 3];
-        analyzer(col, x, y);
-      }
+        p = 4 * (clipX + w * y);
+        for (let x = clipX; x < xmax; x++) {
+            col[0] = data[p];
+            col[1] = data[p + 1];
+            col[2] = data[p + 2];
+            col[3] = data[p + 3];
+            analyzer(col, x, y);
+        }
     }
 }
 
@@ -60,22 +60,22 @@ export function reducePixels<T>(reducer: PixelReducer<T>, initial: T): T {
     return result;
 }
 
-export function filter(filterFunc: Filter, buffer = false): void {
+export function filter(filterFunc: Filter, buffer = false, interpolate = false, enforceBounds = false): void {
     const w = currentCanvas.width, h = currentCanvas.height;
     const dataObj = currentContext.getImageData(0, 0, w, h);
     const data = dataObj.data;
     let p = 0;
 
-    const pixelGetter = prepareBufferOrGetter(buffer ? BUFFER_TYPE.BUFFER : BUFFER_TYPE.NONE, dataObj);
+    const pixelGetter = prepareBufferOrGetter(dataObj, buffer, interpolate, enforceBounds);
 
     const col: ColorRGBA = [0, 0, 0, 0];
     const example = filterFunc(col, 0, 0, () => [0,0,0,255]);
     if (typeof example === "number") {
-      const original = filterFunc;
-      filterFunc = (col, x, y) => {
-        const c = original(col, x, y, pixelGetter) as number;
-        return [c, c, c, 255];
-      }
+        const original = filterFunc;
+        filterFunc = (col, x, y) => {
+            const c = original(col, x, y, pixelGetter) as number;
+            return [c, c, c, 255];
+        }
     }
     const typedFilterFunc = filterFunc as ((c: ColorRGBA, x: number, y: number, get: GetColor) => ColorRGBA);
 
@@ -83,44 +83,71 @@ export function filter(filterFunc: Filter, buffer = false): void {
     const [clipX, clipY, clipW, clipH] = getClipping();
     const ymax = clipY + clipH, xmax = clipX + clipW;
     for (let y = clipY; y < ymax; y++) {
-      p = 4 * (clipX + w * y);
-      for (let x = clipX; x < xmax; x++) {
-        col[0] = data[p];
-        col[1] = data[p + 1];
-        col[2] = data[p + 2];
-        col[3] = data[p + 3];
-        result = typedFilterFunc(col, x, y, pixelGetter);
-        data[p++] = result[0];
-        data[p++] = result[1];
-        data[p++] = result[2];
-        if (result[3] != null) {
-          data[p++] = result[3];
-        } else {
-          data[p++] = 255;
+        p = 4 * (clipX + w * y);
+        for (let x = clipX; x < xmax; x++) {
+            col[0] = data[p];
+            col[1] = data[p + 1];
+            col[2] = data[p + 2];
+            col[3] = data[p + 3];
+            result = typedFilterFunc(col, x, y, pixelGetter);
+            data[p++] = result[0];
+            data[p++] = result[1];
+            data[p++] = result[2];
+            if (result[3] != null) {
+                data[p++] = result[3];
+            } else {
+                data[p++] = 255;
+            }
         }
-      }
     }
     currentContext.putImageData(dataObj, 0, 0);
 }
 
-function prepareBufferOrGetter(buffer: BUFFER_TYPE, imageData: ImageData): GetColor {
-    const data = imageData.data, w = imageData.width;
-    switch (buffer) {
-        case BUFFER_TYPE.NONE:
-            // No buffering -> just get pixel
-            return (x: number, y: number) => {
+function prepareBufferOrGetter(imageData: ImageData, buffer: boolean, interpolate = false, enforceBounds = false): GetColor {
+    const data = imageData.data, w = imageData.width, h = imageData.height, w1 = w - 1, h1 = h - 1;
+    const buff = buffer ? data.slice() : data;
+    const mode = (interpolate ? 1 : 0) | (enforceBounds ? 2 : 0);
+    const rowOffset = 4 * w;
+    switch (mode) {
+        case 0:
+            // No interpolation, no enforced bounds
+            return (x, y) => {
                 const p = 4 * (x + w * y);
-                return [ data[p], data[p + 1], data[p + 2], data[p + 3] ];
+                return [ buff[p], buff[p + 1], buff[p + 2], buff[p + 3] ];
             }
-        case BUFFER_TYPE.BUFFER:
-            // Simple buffering -> store whole image data
-            {
-                const buff = data.slice();
-                return (x: number, y: number) => {
-                    const p = 4 * (x + w * y);
-                    return [ buff[p], buff[p + 1], buff[p + 2], buff[p + 3] ];
-                }
+        case 1:
+            // Interpolation, no bounds
+            return (x, y) => {
+                const x1 = Math.floor(x), y1 = Math.floor(y), dx = x - x1, dy = y - y1, dx1 = 1 - dx, dy1 = 1 - dy;
+                const pTL = 4 * (x1 + w * y1), pTR = pTL + 4, pBL = pTL + rowOffset, pBR = pBL + 4;
+                return [
+                    dx * (dy * buff[pBR    ] + dy1 * buff[pTR    ]) + dx1 * (dy * buff[pBL    ] + dy1 * buff[pTL    ]),
+                    dx * (dy * buff[pBR + 1] + dy1 * buff[pTR + 1]) + dx1 * (dy * buff[pBL + 1] + dy1 * buff[pTL + 1]),
+                    dx * (dy * buff[pBR + 2] + dy1 * buff[pTR + 2]) + dx1 * (dy * buff[pBL + 2] + dy1 * buff[pTL + 2]),
+                    dx * (dy * buff[pBR + 3] + dy1 * buff[pTR + 3]) + dx1 * (dy * buff[pBL + 3] + dy1 * buff[pTL + 3]),
+                ];
             }
+        case 2:
+            // No interpolation, enforced bounds
+            return (x, y) => {
+                const p = 4 * (clamp(x, 0, w) + w * clamp(y, 0, h));
+                return [ buff[p], buff[p + 1], buff[p + 2], buff[p + 3] ];
+            }
+        case 3:
+            // Interpolation, enforced bounds
+            return (x, y) => {
+                const x1 = Math.floor(clamp(x, 0, w1)), y1 = Math.floor(clamp(y, 0, h1)), dx = x - x1, dy = y - y1, dx1 = 1 - dx, dy1 = 1 - dy;
+                const pTL = 4 * (x1 + w * y1), pTR = pTL + 4, pBL = pTL + rowOffset, pBR = pBL + 4;
+                return [
+                    dx * (dy * buff[pBR    ] + dy1 * buff[pTR    ]) + dx1 * (dy * buff[pBL    ] + dy1 * buff[pTL    ]),
+                    dx * (dy * buff[pBR + 1] + dy1 * buff[pTR + 1]) + dx1 * (dy * buff[pBL + 1] + dy1 * buff[pTL + 1]),
+                    dx * (dy * buff[pBR + 2] + dy1 * buff[pTR + 2]) + dx1 * (dy * buff[pBL + 2] + dy1 * buff[pTL + 2]),
+                    dx * (dy * buff[pBR + 3] + dy1 * buff[pTR + 3]) + dx1 * (dy * buff[pBL + 3] + dy1 * buff[pTL + 3]),
+                ];
+            }
+        default:
+            // Should never happen
+            return (x, y) => [0, 0, 0, 0];
     }
 }
 
